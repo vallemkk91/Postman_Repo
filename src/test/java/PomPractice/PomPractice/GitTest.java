7f845d965f3ab3804652351b18a072e72c100dc1
vallemkk91@gmail.com <vallemkk91@gmail.com>
2024-04-01 23:45:52 +0530

    Hello World commit

--- src/test/java/PomPractice/PomPractice/GitTest.java
+++ src/test/java/PomPractice/PomPractice/GitTest.java
@@ -3,7 +3,7 @@ package PomPractice.PomPractice;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 
 	}
 
